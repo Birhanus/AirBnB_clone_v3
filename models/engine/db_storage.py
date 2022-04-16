af3cc9f110d5cd0854179645315bc8cfe48ec6b8
birhanus <birhanushimles716@gmail.com>
2022-04-16 08:11:53 +0000

    update db strage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,18 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ Returns the object based on the class and its ID,
+            or None if not found
+        """
+        all_obj = self.all(cls)
+        key = "{}.{}".format(cls, id)
+        return (all_obj.get(key))
+
+    def count(self, cls=None):
+        """ Returns the number of objects in storage matching the given class.
+            If no class is passed,
+            returns the count of all objects in dbstorage
+        """
+        return len(self.all(cls))
