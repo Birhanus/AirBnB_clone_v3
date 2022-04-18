692cada9c7309b247d7fea8c81a454283e0b7b63
der-bew <derbew12@gmail.com>
2022-04-18 22:40:36 +0300

    Update: index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,34 @@
 #!/usr/bin/python3
-""" status of api"""
+"""index.py to connect to API"""
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
 from models import storage
-from flask import jsonify
 
 
-@app_views.route('/status')
-def response():
-    """ get status ok
-    """
-    dic = {"status": "OK"}
-    return jsonify(dic)
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
-@app_views.route('/stats')
-def class_counter():
-    """ get a dictionary from count method
-    """
-    dic = {}
-    dic["amenities"] = storage.count("Amenity")
-    dic["cities"] = storage.count("City")
-    dic["places"] = storage.count("Place")
-    dic["reviews"] = storage.count("Review")
-    dic["states"] = storage.count("State")
-    dic["users"] = storage.count("User")
-    return jsonify(dic)
-
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ Status of API """
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
 
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
 
-    return jsonify(num_objs)
+if __name__ == "__main__":
+    pass
