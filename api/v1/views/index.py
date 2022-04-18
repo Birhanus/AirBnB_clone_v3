967d76a03dd8c8c70acdad4e815aade39ed10ad1
der-bew <derbew12@gmail.com>
2022-04-18 16:00:47 +0300

    Update: index.py file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,31 @@
 
 from models import storage
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, Blueprint, jsonify
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ Status of API """
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
