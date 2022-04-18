519f54f3a71c21202d61f93252d9434edac74851
der-bew <derbew12@gmail.com>
2022-04-18 23:29:52 +0300

    Update: places_amenities.py file

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" Place_amenities Restful API
-"""
-
+"""places_amenities.py"""
 import os
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
