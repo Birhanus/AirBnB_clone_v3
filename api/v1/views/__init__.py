f37cc32b6416a5f1086267f2fbca4c2e41295f46
birhanus <birhanushimles716@gmail.com>
2022-04-18 14:47:55 +0000

    Diesign api

--- api/v1/views/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" views """
-
-from flask import Blueprint, render_template, abort
-from api.v1.views.index import *
-
-app_views = Blueprint('app_views', url_prefix='/api/v1')
-
